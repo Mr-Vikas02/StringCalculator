c4fb14936999c1f4a268a7e8001c32deafe4a35d
vikasjaiswal693@gmail.com <Vikas@020296>
2024-10-06 11:42:10 +0530

    Commit to allow the add method to handle new lines between numbers (instead of commas). ("1\n2,3" should return 6).

--- src/main/java/com/incubyte/calculator/StringCalculator.java
+++ src/main/java/com/incubyte/calculator/StringCalculator.java
@@ -6,12 +6,21 @@ public class StringCalculator {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-
+		// To build new string with new line
+		StringBuilder sb = new StringBuilder();
 		System.out.println("Enter string: ");
-		StringCalculator strCalc = new StringCalculator();
 
-		String str = scanner.nextLine();
-		System.out.println("Result: " + strCalc.Add(str));
+		String strLine;
+		/**
+		 * Iteration for each new line
+		 * */
+		while (!(strLine = scanner.nextLine()).isEmpty()){
+			sb.append(strLine).append("\n");
+		}
+
+		String finalStr = sb.toString().trim();
+		StringCalculator strCalc = new StringCalculator();
+		System.out.println("Result: " + strCalc.Add(finalStr));
 
 		scanner.close();
 	}
@@ -20,9 +29,8 @@ public class StringCalculator {
 		if (string == null || string.isEmpty())
 			return 0;
 
-		String[] numbers = string.split(",");
+		String[] numbers = string.split("[,\n]");
 		int sum = 0;
-		
 
         for (String num : numbers) {
             num = num.trim();
