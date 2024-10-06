61378871cd20e8b8210bb0e1043445c8d9d31e67
vikasjaiswal693@gmail.com <Vikas@020296>
2024-10-06 11:19:50 +0530

    Commit to allow the add method to handle any amount of numbers.

--- src/main/java/com/incubyte/calculator/StringCalculator.java
+++ src/main/java/com/incubyte/calculator/StringCalculator.java
@@ -24,26 +24,14 @@ public class StringCalculator {
 		int sum = 0;
 		
 
-//		If given string is comma separated string length is greater than 2.
-
-		if (numbers.length > 2) return 0;
-
-		if (numbers.length == 1) {
-			String num = numbers[0].trim();
-			if (!num.isEmpty()) {
-				return sumOfDigits(num);
-			}
-		} else if (numbers.length == 2) {
-			for (String num : numbers) {
-				num = num.trim();
-				if (!num.isEmpty()) {
-					sum += sumOfDigits(num);
-				}
-			}
-			return sum;
-		}
-		return 0;
-	}
+        for (String num : numbers) {
+            num = num.trim();
+            if (!num.isEmpty()) {
+                sum += sumOfDigits(num);
+            }
+        }
+        return sum;
+    }
 
 	/**
 	 * Method used for sum operation.
